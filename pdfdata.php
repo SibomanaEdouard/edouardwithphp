7637094e469593b39439677f6eb5f41de320b5c9
SIBOMANA Edouard <edouard@kali>
2023-05-05 17:58:54 +0200

    To update

--- pdfdata.php
+++ pdfdata.php
@@ -1,50 +1,42 @@
+
 <?php
-// Set headers to indicate that we are outputting a PDF
-header('Content-Type: application/pdf');
-header('Content-Disposition: inline; filename="my_document.pdf"');
-
-// Connect to database
 include("connection.php");
-// Check connection
-if (!$connect) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-// Retrieve data from database
-$sql = "SELECT * FROM users";
-$result = mysqli_query($connnect, $sql);
-
-// Create new PDF document
-$pdf = pdf_new();
-pdf_open_file($pdf);
-
-// Add new page
-pdf_begin_page($pdf, 595, 842);
-$font = pdf_findfont($pdf, 'Helvetica-Bold', 'host', 0);
-pdf_setfont($pdf, $font, 18);
-
-// Loop through data and add to PDF
-$y = 750;
-while ($row = mysqli_fetch_assoc($result)) {
-    pdf_show_xy($pdf, $row['name'], 50, $y);
-    pdf_show_xy($pdf, $row['email'], 200, $y);
-    $y -= 20;
-}
-
-pdf_end_page($pdf);
-
-// Close PDF document and output to browser
-pdf_close($pdf);
-echo pdf_get_buffer($pdf);
-
-// Close database connection
-mysqli_close($connect);
-
-
-// 
-
-
-
-
-
+$sql="SELECT * FROM users";
+$result=$connect->query($sql);
+$data=mysqli_fetch_all($result,MYSQLI_ASSOC);
 ?>
+
+<table id="users" border="1">
+    <thead>
+        <tr>
+            <th>FIRSTNAME</th>
+            <th>LASTNAME</th>
+            <th>EMAIL</th>
+            <th>GENDER</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($data as $rows): ?>
+            <tr>
+                <td><?php echo $rows['firstname']?></td>
+                <td><?php echo $rows['lastname']?></td>
+                <td><?php echo $rows['email']?></td>
+                <td><?php echo $rows['gender']?></td>
+               
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.3.2/html2canvas.min.js"></script>
+ <script>
+    const doc = new jsPDF();
+    const table = document.getElementById('users');
+    html2canvas(table).then(function(canvas){
+        console.log(canvas)
+        var imageData = canvas.toDataURL('image/png');
+        doc.addImage(imageData, 'PNG', 10, 10);
+        doc.save('users.pdf');
+    });
+</script> 
+
